6fbd76560929e9ec6a8e9254fa596bf722f43a00
Dev-Pranshu <daspranshu07@gmail.com>
2026-05-24 20:19:53 +0530

    fix: add CORS headers to allow client requests

--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
+// Allow requests from the client
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 // Create HTTP server and attach WebSocket to it
 const server = http.createServer(app);
